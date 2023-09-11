560762f253f7045df7369f45bd43d085cc33d907
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 12:07:58 -0300

    Mudanças

--- Projeto15.py
+++ Projeto15.py
@@ -2,9 +2,10 @@ brancos = int(input('Insira o Total de votos brancos:'))
 nulos = int(input('Insira o Total de votos nulos:'))
 validos = int(input('Insira o Total de votos validos:'))
 
-total = brancos + nulos + validos / 100
-brancos = total - brancos / 100
-nulos = total - nulos / 100
-validos = total - validos / 100
+total = brancos + nulos + validos
 
-print(f'O total de votos brancos foi {brancos}; \r\nO total de votos nulos foi {nulos}; \r\nO total de votos validos foram {validos}.')
+brancos = brancos * 100 / total
+nulos = nulos * 100 / total
+validos = validos * 100 / total
+
+print(f'O total de votos brancos foi {round(brancos, 2)}; \r\nO total de votos nulos foi {round(nulos, 2)}; \r\nO total de votos validos foram {round(validos, 2)}.')
